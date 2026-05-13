de6529863482329c6ca87df527568dd5b476b854
13unk <treceunk@gmail.com>
2026-05-13 06:02:25 +0200

    update whatsapp link

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_LINK = "https://chat.whatsapp.com/CRBFrwZ3771FPqTAPR6iE7";
+export const WHATSAPP_LINK = "https://chat.whatsapp.com/FDCp6WJRjZXH9NeBE5sdNA";
 
 export const PAINTING_IMAGES = [
   "https://i.postimg.cc/y86zcVKC/cuadro56.jpg",
