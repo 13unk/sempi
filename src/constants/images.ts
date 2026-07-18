000302b22552d0b4b2332bac34e4f10a48e09ba1
juanfdzz <116915178+juanfdzz@users.noreply.github.com>
2026-07-18 19:59:00 +0200

    Update images.ts

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_LINK = "https://chat.whatsapp.com/HqKikl0hsBaKrPeKZe9jH0";
+export const WHATSAPP_LINK = "https://chat.whatsapp.com/K2HHn7BREbA6CUEGQs7B8g";
 
 export const PAINTING_IMAGES = [
   "https://i.postimg.cc/y86zcVKC/cuadro56.jpg",
